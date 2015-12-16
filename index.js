469b36b00e90aa4a6cbcb8654cc75be4d9279613
Nolan Hager <jeffery.hager.dev@gmail.com>
2015-12-16 10:59:02 -0600

    Returns ENOENT error if directory doesn't exist

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 "use strict";
 
-var fs = require("fs");
+var fs = require("fs"),
+  mongodb = require("mongodb"),
+  _ = require("underscore");
 
-var LoadTypes = {
-  directory: function(host, db, seedLocation, callback){}
+var loadActions = {
+  "dir": function(host, db, seedLocation, callback){
+    fs.readdir(seedLocation, function(err, files){
+      if(err){
+        return callback(err);
+      }
+
+
+
+    });
+  },
+  "jsonFile": function(){
+    throw new Error("Not Implemented");
+  },
+  "mongoDump": function(){
+    throw new Error("Not Implemented");
+  }
 };
 
-var loadTypes = ["directory"];
 module.exports = {
   "load": function(host, db, seedLocation, loadType, callback){
-    var action = loadType[loadType];
+    var action = loadActions[loadType];
     if(!action){
-      return callback(new Error("Invalid load type"), null);
+      return callback(new Error("Invalid load type"));
     }
 
     action(host, db, seedLocation, callback);
--- test/loadDirectorySeed.test.js
+++ test/loadDirectorySeed.test.js
@@ -6,15 +6,17 @@ describe("Should Seed a mongo Database based off a directory of JSON Files", fun
   it("Should return an error for invalid load types", function(done){
     mongoSeed.load("localhost", "PetStore", "turtles", "power", function(err, results){
       expect(err).not.to.equal(null);
-      expect(results).to.equal(null);
+      expect(err.message).to.equal("Invalid load type");
+      expect(results).to.undefined;
       done();
     });
   });
 
   it("Should return an error if the directory doesn't exist", function(done){
-    mongoSeed.load("localhost", "PetStore", "turtles", "power", function(err, results){
+    mongoSeed.load("localhost", "PetStore", "turtles", "dir", function(err, results){
       expect(err).not.to.equal(null);
-      expect(results).to.equal(null);
+      expect(err.code).to.equal("ENOENT");
+      expect(results).to.undefined;
       done();
     });
   });
