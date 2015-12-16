49526122cd9d2d48c2942d735a9a795dfe7ace33
Nolan Hager <jeffery.hager.dev@gmail.com>
2015-12-16 14:03:09 -0600

    Loading from a directory of json files passes first round of testing

--- index.js
+++ index.js
@@ -1,37 +1,66 @@
 "use strict";
 
-var fs = require("fs"),
+var fs = require('fs'),
+  path = require('path'),
   mongodb = require("mongodb"),
-  _ = require("underscore");
+  ObjectID = require('mongodb').ObjectID,
+  json2Mongo = require('json2Mongo'),
+  MongoClient = mongodb.MongoClient,
+  async = require('async'),
+  _ = require('underscore');
 
 var loadActions = {
-  "dir": function(host, db, seedLocation, callback){
-    fs.readdir(seedLocation, function(err, files){
-      if(err){
+  "dir": function (host, port, db, seedLocation, callback) {
+    fs.readdir(seedLocation, function (err, files) {
+      if (err) {
         return callback(err);
       }
 
 
+      async.each(files, function (file, done) {
+        if (path.extname(file) != '.json') done(new Error("Invla"));
 
+        var collectionName = path.basename(file, '.json');
+        var content = fs.readFileSync(seedLocation + '/' + file);
+        if (!content) return done();
+
+        content = JSON.parse(content);
+
+        MongoClient.connect('mongodb://' + host + ':' + port + '/' + db, function (err, db) {
+          db.createCollection(collectionName, function (err, col) {
+            db.collection(collectionName, function(err, collection){
+
+              content = json2Mongo(content);
+
+              collection.insertMany(content,{w:1}, function (err) {
+                done(err);
+              });
+            });
+
+          });
+
+        });
+      }, callback);
     });
   },
-  "jsonFile": function(){
+  "jsonFile": function () {
     throw new Error("Not Implemented");
   },
-  "mongoDump": function(){
+  "mongoDump": function () {
     throw new Error("Not Implemented");
   }
 };
 
 module.exports = {
-  "load": function(host, db, seedLocation, loadType, callback){
+  "load": function (host, port, db, seedLocation, loadType, callback) {
     var action = loadActions[loadType];
-    if(!action){
+    if (!action) {
       return callback(new Error("Invalid load type"));
     }
 
-    action(host, db, seedLocation, callback);
+    action(host, port, db, seedLocation, callback);
 
   },
-  "clear": function(host, db, callback){}
+  "clear": function (host, port, db, callback) {
+  }
 };
--- test/loadDirectorySeed.test.js
+++ test/loadDirectorySeed.test.js
@@ -1,24 +1,29 @@
 var mongoSeed = require('../index.js'),
   expect = require('chai').expect;
 
-describe("Should Seed a mongo Database based off a directory of JSON Files", function(){
-
-  it("Should return an error for invalid load types", function(done){
-    mongoSeed.load("localhost", "PetStore", "turtles", "power", function(err, results){
+describe("Should Seed a mongo Database based off a directory of JSON Files", function () {
+  it("Should return an error for invalid load types", function (done) {
+    mongoSeed.load("localhost",27017, "PetStore", "turtles", "power", function (err, results) {
       expect(err).not.to.equal(null);
       expect(err.message).to.equal("Invalid load type");
-      expect(results).to.undefined;
+      expect(results).to.be.undefined;
       done();
     });
   });
 
-  it("Should return an error if the directory doesn't exist", function(done){
-    mongoSeed.load("localhost", "PetStore", "turtles", "dir", function(err, results){
+  it("Should return an error if the directory doesn't exist", function (done) {
+    mongoSeed.load("localhost",27017, "PetStore", "turtles", "dir", function (err, results) {
       expect(err).not.to.equal(null);
       expect(err.code).to.equal("ENOENT");
-      expect(results).to.undefined;
+      expect(results).to.be.undefined;
       done();
     });
   });
 
+  it("Should populate a database given a directory full of JSON exports of collections", function (done) {
+    mongoSeed.load("localhost",27017, "PetStore", __dirname+"/seeds/dirSeed", "dir", function (err, results) {
+      expect(err).to.equal(null);
+      done();
+    });
+  });
 });
