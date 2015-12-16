c1678bb0a3303982d70212ff3a56a5656694faed
Nolan Hager <jeffery.hager.dev@gmail.com>
2015-12-16 14:13:04 -0600

    Updating the readme and finishing the test file

--- README.md
+++ README.md
@@ -10,3 +10,32 @@ A little node module that will allow a user to clear a mongo database and then p
 This is where you have a directory filled with JSON files that were created from a ```mongoexport```.
 
 _NOTE_: make sure to use --jsonArray when exporting data from tables. Similar ```mongoexport --db PetShop --collection Food --out Food.json --jsonArray```
+
+
+```javascript
+mongoSeed.load("localhost",27017, "<name_of_database>", "<seed_directory>", "dir", function (err) {
+  //..do what ever you need
+});
+```
+
+## File
+
+A file is similar to the directory seed. You have a file with an object in it where each property is a collection name and the value is an array of documents for that collection.
+
+```json
+{
+    "table_Name": [/*Each document as an individual object in the array*/]
+    /*...*/
+    "table_Name_n": [/*Each document as an individual object in the array*/]
+}
+```
+
+```javascript
+mongoSeed.load("localhost",27017, "<name_of_database>", "<path_to_file>", "file", function (err) {
+  //..do what ever you need
+});
+```
+
+## Mong Dump file
+
+Comming soon
--- test/loadDirectorySeed.test.js
+++ test/loadDirectorySeed.test.js
@@ -3,25 +3,23 @@ var mongoSeed = require('../index.js'),
 
 describe("Should Seed a mongo Database based off a directory of JSON Files", function () {
   it("Should return an error for invalid load types", function (done) {
-    mongoSeed.load("localhost",27017, "PetStore", "turtles", "power", function (err, results) {
+    mongoSeed.load("localhost",27017, "PetStore", "turtles", "power", function (err) {
       expect(err).not.to.equal(null);
       expect(err.message).to.equal("Invalid load type");
-      expect(results).to.be.undefined;
       done();
     });
   });
 
   it("Should return an error if the directory doesn't exist", function (done) {
-    mongoSeed.load("localhost",27017, "PetStore", "turtles", "dir", function (err, results) {
+    mongoSeed.load("localhost",27017, "PetStore", "turtles", "dir", function (err) {
       expect(err).not.to.equal(null);
       expect(err.code).to.equal("ENOENT");
-      expect(results).to.be.undefined;
       done();
     });
   });
 
   it("Should populate a database given a directory full of JSON exports of collections", function (done) {
-    mongoSeed.load("localhost",27017, "PetStore", __dirname+"/seeds/dirSeed", "dir", function (err, results) {
+    mongoSeed.load("localhost",27017, "PetStore", __dirname+"/seeds/dirSeed", "dir", function (err) {
       expect(err).to.equal(null);
       done();
     });
