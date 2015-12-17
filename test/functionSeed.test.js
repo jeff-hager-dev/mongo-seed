5cb98af1d34910e8b1fd30716b21e50527b9889e
Nolan Hager <jeffery.hager.dev@gmail.com>
2015-12-16 18:51:07 -0600

    Defining the test cases for seeding by function

---
+++ test/functionSeed.test.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var mongoSeed = require('../index.js'),
+  Db = require('mongodb').Db,
+  Server = require('mongodb').Server,
+  expect = require('chai').expect;
+
+describe("Seed a Mongo Database based off a function", function () {
+  var host = "localhost";
+  var port = 27017;
+  var dbName = "PetStore";
+
+  before(function (done) {
+    var db = new Db(dbName, new Server(host, port));
+    db.open(function (err, db) {
+      db.dropDatabase(function () {
+        done();
+      });
+    });
+  });
+
+  it("Should return an error if the file with the function doesn't exist", function (done) {
+    mongoSeed.load(host, port, dbName, __dirname + "/seeds/functionSeed_foo.json", "file", function (err) {
+      expect(err).not.to.equal(null);
+      done();
+    });
+  });
+
+  it("Should populate a database given a directory full of JSON exports of collections", function (done) {
+    mongoSeed.load(host, port, dbName, __dirname + "/seeds/functionSeed.json", "file", function (err) {
+      expect(err).to.equal(null);
+      done();
+    });
+  });
+
+});
